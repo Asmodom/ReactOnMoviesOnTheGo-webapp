a05bb0f4c6ad7b176ab9c2db3da1dd04003648a6
Asmodom <donny.nijssen@gmail.com>
2022-04-30 16:14:14 +0200

    finished initial app flow

--- reactmovies-to-go/src/App.js
+++ reactmovies-to-go/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
-import React, {Fragment} from 'react';
-import {HashRouter as Router, Switch, Route, Link, useParams } from 'react-router-dom';
+import React, { Fragment } from 'react';
+import { HashRouter as Router, Switch, Route, Link, useParams, useRouteMatch } from 'react-router-dom';
 import Movies from './components/Movies';
 import Home from './components/Home';
 import Admin from './components/Admin';
+import Categories from './components/Categories';
+import OneMovie from './components/OneMovie';
 
 export default function App() {
   return (
@@ -12,7 +14,7 @@ export default function App() {
         <h1 className="mt-3">
           Go watch a movie or serie
         </h1>
-        <hr className="mb-3"/>
+        <hr className="mb-3" />
       </div>
 
       <div className="row">
@@ -26,6 +28,9 @@ export default function App() {
                 <Link to="/movies">Movies</Link>
               </li>
               <li className="list-group-item">
+                <Link to="/by-category">Categories</Link>
+              </li>
+              <li className="list-group-item">
                 <Link to="/admin">Manage Catalogue</Link>
               </li>
             </ul>
@@ -34,17 +39,29 @@ export default function App() {
 
         <div className="col-md-10">
           <Switch>
-            <Route path="/movies/:id">
-              <Movie/>
-            </Route>
+            <Route path="/movies/:id" component={OneMovie}/>
             <Route exact path="/movies">
-              <Movies/>
+              <Movies />
             </Route>
+            <Route exact path="/by-category">
+              <CategoryPage />
+            </Route>
+            <Route
+              exact
+              path="/by-category/drama"
+              render={(props) => <Categories {...props} title={`Drama`} />} />
+
+            <Route
+              exact
+              path="/by-category/comedy"
+              render={(props) => <Categories {...props} title={`Comedy`} />} />
+
+
             <Route exact path="/admin">
-              <Admin/>
+              <Admin />
             </Route>
             <Route exact path="/">
-              <Home/>
+              <Home />
             </Route>
           </Switch>
         </div>
@@ -55,7 +72,15 @@ export default function App() {
 }
 
 
-function Movie(){
-  let {id} = useParams();
-  return <h2>Movie id {id}</h2>
+function CategoryPage() {
+  let { path, url } = useRouteMatch();
+
+  return <div>
+    <h2>Categories</h2>
+    <ul>
+      <li><Link to={`${path}/drama`}>Drama</Link></li>
+      <li><Link to={`${path}/comedy`}>Comedy</Link></li>
+      <li><Link to={`${path}/horror`}>Horror</Link></li>
+    </ul>
+  </div>;
 }
