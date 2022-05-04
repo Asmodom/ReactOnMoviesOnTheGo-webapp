c18cbb67fe1f7673f07b68ad748def2d22cef4ba
Asmodom <donny.nijssen@gmail.com>
2022-05-04 23:04:50 +0200

    added a small refactoring to display genres

---
+++ reactmovies-to-go/src/components/Genres.js
@@ -0,0 +1,55 @@
+import React, {Component, Fragment}from 'react';
+import {Link} from 'react-router-dom';
+
+export default class Genres extends Component{
+
+    state = {
+        genres: [],
+        isLoaded: false,
+        error: null
+    };
+
+    componentDidMount() {
+        fetch("http://localhost:4000/v1/genress")
+        //.then(response => response.json())
+        .then((response) =>{
+            console.log("Status code is", response.status);
+            if(response.status !== "200"){
+                let err = Error;
+                err.message = "Invalid rsponse code: "+ response.status;
+                this.setState({error: err});
+            }
+            return response.json();
+        })
+        .then((json) =>{
+            this.setState({
+                genres: json.genrees,
+                isLoaded: true,
+            },
+            (error)=>{
+                this.setState({
+                    isLoaded: true,
+                    error
+                })
+            });
+        });
+    }
+
+    render(){
+        const {genres, isLoaded, error} = this.state;
+
+        return <Fragment>
+            <h2>Genres</h2>
+            <ul>
+                {genres.map((g) => (
+                    <li key={g.id}>
+                        <Link to={`/genre/${g.id}`}>
+                            {g.genre_name}
+                        </Link>
+                    </li>
+                ))}
+
+            </ul>
+        </Fragment>
+    }
+}
